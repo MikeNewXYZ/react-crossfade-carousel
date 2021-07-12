30777611b1f49d4ee3ae5e18e993892706ab7b49
NotBaldrick <Account@NotBaldrick.com>
2021-07-12 11:34:47 +0100

    working version

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,120 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CrossfadeCarousel from '@notbaldrick/react-crossfade-carousel'
+import styled from 'styled-components'
 
-import { ExampleComponent } from '@notbaldrick/react-crossfade-carousel'
-import '@notbaldrick/react-crossfade-carousel/dist/index.css'
+const StyContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+const StyOptionsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 1.5rem;
+  position: absolute;
+  z-index: 100;
+  background-color: rgb(255 255 255 / 25%);
+  backdrop-filter: blur(5px);
+  padding: 1rem;
+  margin-bottom: 2rem;
+  border-radius: 0.25rem;
+  box-shadow: 0 0.25rem 0.25rem rgb(0 0 0 / 25%);
+`
+
+const StyHeader = styled.div`
+  font-size: 3rem;
+  font-weight: bold;
+`
+
+const StyOptionControl = styled.div`
+  width: 100%;
+`
+
+const StyButton = styled.button`
+  width: 100%;
+  height: 3rem;
+  font-size: 2rem;
+  cursor: pointer;
+  border: none;
+  border-radius: 0.25rem;
+  outline: none;
+`
+
+const StyInput = styled.input`
+  width: 100%;
+  height: 3rem;
+  font-size: 2rem;
+  border: none;
+  border-radius: 0.25rem;
+  outline: none;
+`
+
+const images = [
+  'https://picsum.photos/id/118/1500/1000',
+  'https://picsum.photos/id/120/4928/3264',
+  'https://picsum.photos/id/127/4032/2272'
+]
+
+function App() {
+  const [forceActiveImage, setForceActiveImage] = useState(-1)
+  const [interval, setInterval] = useState(5000)
+  const [transition, setTransition] = useState(5000)
+  const [cycle, setCycle] = useState(true)
+
+  return (
+    <StyContainer>
+      <StyOptionsContainer>
+        <StyHeader>Carousel Options</StyHeader>
+        <StyOptionControl>
+          <p>props.cycle: {cycle ? 'true' : 'false'}</p>
+          <StyButton onClick={() => setCycle(!cycle)}>
+            {cycle ? 'Pause' : 'Play'} Carousel
+          </StyButton>
+        </StyOptionControl>
+        <StyOptionControl>
+          <p>
+            props.forceActiveImage:{' '}
+            {forceActiveImage < 0 ? 'null' : forceActiveImage}
+          </p>
+          <StyInput
+            type='number'
+            value={forceActiveImage}
+            onChange={(e) => setForceActiveImage(e.target.value)}
+            max={images.length - 1}
+            min={-1}
+          />
+        </StyOptionControl>
+        <StyOptionControl>
+          <p>props.interval: {interval}</p>
+          <StyInput
+            type='number'
+            value={interval}
+            onChange={(e) => setInterval(e.target.value)}
+          />
+        </StyOptionControl>
+        <StyOptionControl>
+          <p>props.transition: {transition}</p>
+          <StyInput
+            type='number'
+            value={transition}
+            onChange={(e) => setTransition(e.target.value)}
+          />
+        </StyOptionControl>
+      </StyOptionsContainer>
+      <CrossfadeCarousel
+        forceActiveImage={forceActiveImage < 0 ? null : forceActiveImage}
+        interval={interval}
+        transition={transition}
+        cycle={cycle}
+        images={images}
+      />
+    </StyContainer>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,78 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const StyContainer = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+`
+
+const StyImage = styled.div`
+  position: absolute;
+  width: inherit;
+  height: inherit;
+  background-size: cover;
+  background-position: center;
+`
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+function CrossfadeCarousel({ ...props }) {
+  const [active, setActive] = useState(
+    props.forceActiveImage ? parseInt(props.forceActiveImage) : 0
+  )
+  const [firstTransitionIsDone, setFirstTransitionIsDone] = useState(false)
+
+  useEffect(() => {
+    if (props.forceActiveImage) return
+    if (!props.cycle) return
+    const timeout = setTimeout(() => {
+      async function startImageTransition() {
+        if (firstTransitionIsDone) await wait(props.transition)
+        setActive(active === props.images.length - 1 ? 0 : active + 1)
+        setFirstTransitionIsDone(true)
+      }
+      if (props.cycle) {
+        startImageTransition()
+      }
+    }, props.interval)
+
+    return () => clearTimeout(timeout)
+  }, [active, firstTransitionIsDone, props])
+
+  useEffect(() => {
+    if (!props.forceActiveImage) return
+    setActive(parseInt(props.forceActiveImage))
+  }, [active, props.forceActiveImage])
+
+  return (
+    <StyContainer {...props}>
+      {props.images.map((image, index) => (
+        <StyImage
+          key={`${image}-${index}`}
+          style={{
+            backgroundImage: `url(${image})`,
+            opacity: active === index ? 1 : 0,
+            transition: `opacity ${props.transition}ms`,
+            ...props.imageStyles
+          }}
+          {...props.imageProps}
+        />
+      ))}
+    </StyContainer>
+  )
 }
+
+CrossfadeCarousel.defaultProps = {
+  forceActiveImage: null,
+  cycle: true,
+  interval: 5000,
+  transition: 5000,
+  images: [
+    'https://picsum.photos/id/118/1500/1000',
+    'https://picsum.photos/id/120/4928/3264',
+    'https://picsum.photos/id/127/4032/2272'
+  ]
+}
+
+export default CrossfadeCarousel
